81ef01788c01120a97ef638093a80fe30c6d9afa
Chris Tack <chris_tack@mac.com>
2018-11-09 10:57:22 -0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Project-3-Assessment' });
+});
+
+router.get('/posts', function(req, res, next) {
+  res.render('posts');
+});
+
+module.exports = router;
